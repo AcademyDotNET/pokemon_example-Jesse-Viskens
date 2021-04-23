95f7c382b2fea2a2d99322cf63b9181c30d5e860
Jesse <jesvis@bitconsult.be>
2021-04-23 14:49:03 +0200

    Add project files.

---
+++ Pokemon/Pokemon.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon
+{
+    class Pokemon
+    {
+        private int hp_Base, attack_Base, defense_Base, specialAttack_Base, specialDefense_Base, speed_Base = 0;
+        private int level = 1;
+        //full props
+        public int Hp_Base
+        {
+            get
+            {
+                return hp_Base;
+            }
+            set
+            {
+                hp_Base = value;
+            }
+        }
+        public int Attack_Base
+        {
+            get { return attack_Base; }
+            set { attack_Base = value; }
+        }
+        public int Defense_Base
+        {
+            get { return defense_Base; }
+            set { defense_Base = value; }
+        }
+        public int SpecialAttack_Base
+        {
+            get
+            {
+                return specialAttack_Base;
+            }
+            set
+            {
+                specialAttack_Base = value;
+            }
+        }
+        public int SpecialDefence_Base
+        {
+            get { return specialDefense_Base; }
+            set { specialDefense_Base = value; }
+        }
+        public int Speed_Base
+        {
+            get { return speed_Base; }
+            set { speed_Base = value; }
+        }
+        public int Level
+        {
+            get { return level; }
+            private set { level = value; }
+        }
+        //auto props
+        public string Naam { get; set; }
+        public int Nummer { get; set; }
+        public string Type { get; set; }
+        //Statistics
+        public double Average
+        {
+            get
+            {
+                return (Hp_Base + Attack_Base + Defense_Base + SpecialAttack_Base + SpecialDefence_Base + speed_Base) / 6;
+            }
+        }
+        public double Total
+        {
+            get
+            {
+                return Hp_Base + Attack_Base + Defense_Base + SpecialAttack_Base + SpecialDefence_Base + speed_Base;
+            }
+        }
+        //level based stats
+        public int Hp_Full
+        {
+            get
+            {
+                return (((Hp_Base + 50) * Level) / 50) + 10;
+            }
+        }
+        public int Attack_Full
+        {
+            get { return ((Attack_Base * Level) / 50) + 5; }
+        }
+        public int Defense_Full
+        {
+            get { return ((Defense_Base * Level) / 50) + 5; }
+        }
+        public int SpecialAttack_Full
+        {
+            get { return ((SpecialAttack_Base * Level) / 50) + 5; }
+        }
+        public int SpecialDefense_Full
+        {
+            get { return ((SpecialDefence_Base * Level) / 50) + 5; }
+        }
+        public int Speed_Full
+        {
+            get { return ((Speed_Base * Level) / 50) + 5; }
+        }
+        public int Average_Full
+        {
+            get
+            {
+                return (Hp_Full + Attack_Full + Defense_Full + SpecialAttack_Full + SpecialDefense_Full + Speed_Full) / 6;
+            }
+        }
+
+        //methods
+        public void VerhoogLevel()
+        {
+            Level++;
+        }
+
+        //Part 2: the poketester
+        public void ShowInfo()
+        {
+            Console.WriteLine($"" +
+                $"{Naam} (level {Level})\n" +
+                $"\t* Health = {Hp_Base}\n" +
+                $"\t* Attack = {Attack_Base}\n" +
+                $"\t* Defence = {Defense_Base}\n" +
+                $"\t* Special Attack = {SpecialAttack_Base}\n" +
+                $"\t* Special Defence = {SpecialDefence_Base}\n" +
+                $"\t* Speed = {Speed_Base}\n" +
+                $"Full stats:\n" +
+                $"\t* Health = {Hp_Full}\n" +
+                $"\t* Attack = {Attack_Full}\n" +
+                $"\t* Defence = {Defense_Full}\n" +
+                $"\t* Special Attack = {SpecialAttack_Full}\n" +
+                $"\t* Special Defence = {SpecialDefense_Full}\n" +
+                $"\t* Speed = {Speed_Full}\n"
+                );
+        }
+    }
+}
---
+++ Pokemon/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Maak enkele Pokemon aan 
+
+            Pokemon pikachu = new Pokemon();
+            pikachu.Naam = "Pikachu";
+            pikachu.Hp_Base = 35;
+            pikachu.Attack_Base = 55;
+            pikachu.Defense_Base = 40;
+            pikachu.SpecialAttack_Base = 50;
+            pikachu.SpecialDefence_Base = 50;
+            pikachu.Speed_Base = 90;
+            pikachu.ShowInfo();
+
+            Pokemon rattata = new Pokemon();
+            rattata.Naam = "Rattata";
+            rattata.Hp_Base = 30;
+            rattata.Attack_Base = 56;
+            rattata.Defense_Base = 35;
+            rattata.SpecialAttack_Base = 25;
+            rattata.SpecialDefence_Base = 35;
+            rattata.Speed_Base = 72;
+            rattata.ShowInfo();
+
+            Pokemon magikarp = new Pokemon();
+            magikarp.Naam = "Magicarp";
+            magikarp.Hp_Base = 20;
+            magikarp.Attack_Base = 10;
+            magikarp.Defense_Base = 55;
+            magikarp.SpecialAttack_Base = 15;
+            magikarp.SpecialDefence_Base = 20;
+            magikarp.Speed_Base = 80;
+            magikarp.ShowInfo();
+
+            //Toon aan dat de Average, Total , HP en andere stats correct berekend worden
+            Console.WriteLine($"Average Level of {pikachu.Naam}: { pikachu.Average}");
+            Console.WriteLine($"Total Level of {pikachu.Naam}: { pikachu.Total}");
+
+            //Level-up tester
+            for (int i = 0; i< 55; i++)
+            {
+                pikachu.VerhoogLevel();
+            }
+            Random rnd = new Random();
+            //pokemon generator
+            Pokemon specialPokemon = GeneratorPokemon(rnd,"test");
+
+            //battle tester
+            int winner = Battle(pikachu, magikarp);
+
+            //alles samen
+            AllesSamen(rnd);
+
+        }
+
+        private static void AllesSamen(Random rnd)
+        {
+            Pokemon pokemon1 = GeneratorPokemon(rnd, "Kinkysaurus");
+            Pokemon pokemon2 = GeneratorPokemon(rnd, "Charmander");
+            int result = Battle(pokemon1, pokemon2);
+            int aantalLevelsGestegen = rnd.Next(1, 6);
+
+            switch (result)
+            {
+                case 1:
+                    for (int i = 0; i < aantalLevelsGestegen; i++)
+                    {
+                        pokemon1.VerhoogLevel();
+                    }
+                    Console.WriteLine($"Pokemon {pokemon1.Naam} won! He now leveled {aantalLevelsGestegen} levels.");
+                    break;
+                case 2:
+                    for (int i = 0; i < aantalLevelsGestegen; i++)
+                    {
+                        pokemon2.VerhoogLevel();
+                    }
+                    Console.WriteLine($"Pokemon {pokemon2.Naam} won! He now leveled {aantalLevelsGestegen} levels.");
+                    break;
+                case 0:
+                    Console.WriteLine("It's a draw.");
+                    break;
+                default:
+                    Console.WriteLine("I don't know how you got here. ");
+                    break;
+            }
+        }
+
+        //Maak een methode met volgende signatuur: static Pokemon GeneratorPokemon(). 
+        //Plaats deze methode niet in je Pokémon-klasse, maar in Program.cs.
+        public static Pokemon GeneratorPokemon(Random rnd,string name)
+        {
+            Pokemon randomPokemon = new Pokemon();
+
+            randomPokemon.Hp_Base = rnd.Next(1, 251);
+            randomPokemon.Attack_Base = rnd.Next(1, 251);
+            randomPokemon.Defense_Base = rnd.Next(1, 251);
+            randomPokemon.SpecialAttack_Base = rnd.Next(1, 251);
+            randomPokemon.SpecialDefence_Base = rnd.Next(1, 251);
+            randomPokemon.Speed_Base = rnd.Next(1, 251);
+
+            return randomPokemon;
+        }
+        public static int Battle(Pokemon poke1, Pokemon poke2)
+        {
+            if (poke1 ==null && poke2 == null)
+            {
+                //draw
+                return 0;
+            }else if (poke1 == null && poke2 != null)
+            {
+                //poke2 wint
+                return 2;
+            }else if (poke1 != null && poke2 == null)
+            {
+                //poke1 wint
+                return 1;
+            }
+            else
+            {
+                //let them fight! i know, kinda boring but hey :)
+                int winner = poke1.Average_Full - poke2.Average_Full;
+                if(winner < 0)
+                {
+                    return 2;
+                }else if(winner > 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
